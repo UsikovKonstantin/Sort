078b7a12fe97aacf846f5fa32b5952096b58427b
Nydus Born <nydusborn@outlook.com>
2022-04-23 11:34:21 +0300

    Поведение галочек генерации

--- WinFormsSort/Form.cs
+++ WinFormsSort/Form.cs
@@ -7,6 +7,7 @@ namespace WinFormsSort
         public Form()
         {
             InitializeComponent();
+            Update_Gen_Labels();
         }
 
         private void Tx_Test_Count_TextChanged(object sender, EventArgs e)
@@ -41,22 +42,75 @@ namespace WinFormsSort
 
         private void Ch_Int_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (Ch_Int.Checked)
+            {
+                Ch_Long.Checked = false;
+                Ch_Double.Checked = false;
+                Ch_Str.Checked = false;
+            }
+            else if (!Ch_Int.Checked && !Ch_Long.Checked && !Ch_Double.Checked && !Ch_Str.Checked)
+            {
+                Ch_Int.Checked = true;
+            }
+            Update_Gen_Labels();
         }
 
         private void Ch_Long_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (Ch_Long.Checked)
+            {
+                Ch_Int.Checked = false;
+                Ch_Double.Checked = false;
+                Ch_Str.Checked = false;
+            }
+            else if (!Ch_Int.Checked && !Ch_Long.Checked && !Ch_Double.Checked && !Ch_Str.Checked)
+            {
+                Ch_Long.Checked = true;
+            }
+            Update_Gen_Labels();
         }
 
         private void Ch_Double_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (Ch_Double.Checked)
+            {
+                Ch_Long.Checked = false;
+                Ch_Int.Checked = false;
+                Ch_Str.Checked = false;
+            }
+            else if (!Ch_Int.Checked && !Ch_Long.Checked && !Ch_Double.Checked && !Ch_Str.Checked)
+            {
+                Ch_Double.Checked = true;
+            }
+            Update_Gen_Labels();
         }
 
         private void Ch_Str_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (Ch_Str.Checked)
+            {
+                Ch_Long.Checked = false;
+                Ch_Double.Checked = false;
+                Ch_Int.Checked = false;
+            }
+            else if (!Ch_Int.Checked && !Ch_Long.Checked && !Ch_Double.Checked && !Ch_Str.Checked)
+            {
+                Ch_Str.Checked = true;
+            }
+            Update_Gen_Labels();
+        }
+        void Update_Gen_Labels()
+        {
+            if (Ch_Str.Checked)
+            {
+                Lb_Max.Text = "Max length";
+                Lb_Min.Text = "Min length";
+            }
+            else
+            {
+                Lb_Max.Text = "Max value";
+                Lb_Min.Text = "Min value";
+            }
         }
     }
 }
