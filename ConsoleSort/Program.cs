ddbec744f8b6151fbd8eba7f600f003f10f6ec0f
theponylover666 <dotaonelove332@gmail.com>
2022-04-25 20:08:00 +0300

    Добавил UnitTest'ы

--- ConsoleSort/Program.cs
+++ ConsoleSort/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleSort
         static void Main(string[] args)
         {
             var arr = Sort.GenerateArrayInt(1, 100, 100);
-            Sort.MergeSort(arr, true, 0, arr.Length - 1);
+            //Sort.MergeSort(arr, true, 0, arr.Length - 1);
             foreach (var item in arr)
             {
                 Console.WriteLine(item);
